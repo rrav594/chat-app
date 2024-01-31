2a7453a0f5db585e3494629cf8cfc9ef4f1c633b
Ravi Ranjan <rav594.r@gmail.com>
2024-02-01 02:09:41 +0530

    Project setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.get("/", (req, res) => {
+  res.send("hello from ravi ranjan -----");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Server started...");
+});
